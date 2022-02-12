cbafda6fcab52a2b8f8576bc91977f798b601b60
Sol032 <93797789+Sol032@users.noreply.github.com>
2022-02-12 17:49:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace guess_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gameOn = true;
+            string word = "Family";
+            int tries = 5;
+            int guesses = 0;
+
+            while (gameOn)
+            {
+                if (guesses != tries)
+                {
+                    Console.WriteLine("Enter guess");
+                    string guess = Console.ReadLine();
+                    if (guess == word)
+                    {
+                        Console.WriteLine("You won!, the word was " + word);
+                        gameOn = false;
+                    }
+                    else if (guess != word)
+                    {
+                        Console.WriteLine("Wrong guess, try again");
+                        guesses = guesses + 1;
+                    }
+                }
+                else if (guesses == tries)
+                {
+                    Console.WriteLine("You lost!, the word was " + word);
+                    gameOn = false;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
